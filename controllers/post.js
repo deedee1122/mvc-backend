c45c379035b3c2dba013ef5b59909f1b7047df4c
deedee1122 <oojedapo@aol.co.uk>
2024-02-26 10:45:36 +0000

    express in progress

--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,4 @@
+// const { application } = require('express')
 const { Post } = require('../models/index.js')
 
 module.exports.getAll = function (req, res) {
@@ -18,6 +19,48 @@ module.exports.getOne = function (req, res) {
     .catch()
 }
 
- module.exports.createOne = function (req, res){
-  Post.create({ title: })
- }
+module.exports.createOne = function (req, res) {
+  Post.create({ title: req.body.title, text: req.body.text })
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+module.exports.useOne = function (req, res) {
+  const id = req.params.id
+
+  Post.findByPk(id)
+    .then((post) => {
+      post.update({ title: req.body.title, text: req.body.text })
+        .then(() => {
+          res.status(200).json({ post: 'Hello world' })
+        })
+        .catch((error) => {
+          res.status(500).json(error)
+        })
+    })
+
+    .catch((error) => {
+      res.status(500).json(error)
+    })
+}
+
+module.exports.delOne = function (req, res, next) {
+  const id = req.params.id
+  Post.findByPk(id)
+    .then((post) => {
+      if (post == null) {
+        return res.status(404).json({ message: 'Not found' })
+      }
+      post.destroy().then(() => {
+        res.status(200).json({ delete: 'message deleted' })
+      })
+        .catch((error) => {
+          res.status(500).json(error)
+        })
+    })
+    .catch()
+}
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const postRoutes = require('./routes/posts.js')
-const app = express()
 
+const app = express()
+app.use(express.json())
 app.use('/api/v1/post', postRoutes)
+
 const port = 3000
 app.listen(port, () => {
   console.log(`Server started on ${port}`)
